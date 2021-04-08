e39341b23ba4b8dcb15797a5b87d65b0007baf0d
Erik Sandstedt <erik@sandstedt.eu>
2021-04-08 18:33:31 +0200

    updated ui

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Model } from "@esandstedt/stellaris-model";
 import { Loader } from "./components/Loader";
 import Svg from "./components/Svg";
 
+import "./App.css";
+
 const WIDTH = (() => {
   if (typeof window !== "undefined") {
     return Math.min(1200, window.innerWidth - 20);
@@ -16,10 +18,25 @@ function App() {
   const [model, setModel] = useState<Model>();
 
   return (
-    <>
-      {!model && <Loader onLoad={setModel} />}
+    <div className="app container">
+      <div className="header">
+        <h1 className="header-title">Stellaris Map</h1>
+        <div className="header-extra">
+          {model && (
+            <>
+              <button onClick={() => alert("not implemented")}>Download</button>
+              <button onClick={() => setModel(undefined)}>Reset</button>
+            </>
+          )}
+        </div>
+      </div>
+      {!model && (
+        <div className="loader-wrapper">
+          <Loader onLoad={setModel} />
+        </div>
+      )}
       {model && <Svg model={model} width={WIDTH} height={WIDTH} />}
-    </>
+    </div>
   );
 }
 
